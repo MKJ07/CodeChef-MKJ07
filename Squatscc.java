0cef287891b639ccf083df2db37f4ed351b9630a
MKJ07 <mahesh8479j@gmail.com>
2022-11-24 23:45:14 +0530

    24/11/22

---
+++ Squatscc.java
@@ -0,0 +1,21 @@
+import java.util.*;
+/*code link:-https://www.codechef.com/problems/SQUATS
+input:- 3
+1
+4
+99
+output:- 15 60 1485*/
+public class Squatscc {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+		Scanner sc=new Scanner(System.in);
+		int t=sc.nextInt();
+		for(int i=0;i<t;i++){
+		    int n=sc.nextInt();
+		    System.out.println(15*n);
+		}
+        sc.close();
+	}
+}
+
+}
