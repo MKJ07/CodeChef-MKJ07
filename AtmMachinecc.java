04a51c2bb3471d5742ae59f841c50163ae4e2458
Mahesh Kumar Jaiswal <87239664+MKJ07@users.noreply.github.com>
2022-11-27 19:49:23 +0530

    27/11/22

---
+++ AtmMachinecc.java
@@ -0,0 +1,72 @@
+/*input:-2
+
+5 10
+
+3 5 3 2 1
+
+4 6
+
+10 8 6 4
+
+output:- 11010 0010*/
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+class Codechef
+
+{
+
+
+	public static void main (String[] args)	{
+
+
+
+        Scanner sc=new Scanner (System.in);
+
+        int t=sc.nextInt();
+
+        for(int i=0;i<t;i++){
+
+            int n=sc.nextInt ();
+
+            int k=sc.nextInt ();
+
+            int[] arr=new int[n];
+
+            for(int j=0;j<n;j++){
+
+                arr[j]=sc.nextInt();
+
+            }
+
+            String s="";
+
+            for(int m=0;m<arr.length;m++){
+
+                if(k<arr[m]){
+
+                    s+="0";
+
+                }
+
+                else{
+
+                    k=k-arr[m];
+
+                    s+="1";
+
+                }
+
+            }
+
+            System.out.println(s);
+
+        }
+
+	}
+
+}
