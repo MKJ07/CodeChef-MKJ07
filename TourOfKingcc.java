13e1f2076746476f70c60ebf1d48f60c952ac0a2
Mahesh Kumar Jaiswal <87239664+MKJ07@users.noreply.github.com>
2022-12-02 23:23:06 +0530

    2/12/22

---
+++ TourOfKingcc.java
@@ -0,0 +1,45 @@
+/* Code link:-https://www.codechef.com/problems/KNGTOR
+ Input:- 4
+
+4 8
+
+2 13
+
+14 5
+
+8 8
+ Output:- 76 101 105 96*/
+
+import java.util.*;
+
+import java.lang.*;
+
+import java.io.*;
+
+
+
+class Codechef
+
+{
+
+	public static void main (String[] args) throws java.lang.Exception	{
+
+	    Scanner sc=new Scanner (System.in);
+
+	    int t=sc.nextInt();
+
+	    for(int i=0;i<t;i++){
+
+	        int n=sc.nextInt();
+
+	        int m=sc.nextInt();
+
+	        System.out.println((5*n)+(7*m));
+
+	    }
+
+	}
+
+	
+
+}
