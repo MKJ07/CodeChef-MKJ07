6f08dde4c830ed5c8423ad4dd211766f2a7af933
MKJ07 <mahesh8479j@gmail.com>
2023-04-05 22:26:28 +0530

    Contest(Starters 84

---
+++ ChefandDonationcc.java
@@ -0,0 +1,22 @@
+import java.util.*;
+/*Question: Chef and Donation
+Input : 4
+1 3
+2 5
+4 5
+2 10
+
+Output : 2 3 1 8
+ */
+public class ChefandDonationcc {
+    public static void main (String[] args) throws java.lang.Exception
+	{
+	    Scanner sc=new Scanner(System.in);
+	    int t=sc.nextInt();
+		for (int i=0;i<t;i++){
+		    int x=sc.nextInt();
+		    int y=sc.nextInt();
+		    System.out.println(y-x);
+		} 
+	}
+}
